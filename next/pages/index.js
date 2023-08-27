5825743c29f377506087d773d9e165d06a1a4e00
fGWKZUTL0 <onatik0198@gmail.com>
2023-08-27 18:55:23 +0900

    etc/自己紹介修正

--- next/pages/index.js
+++ next/pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData  }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>03生まれ 福岡生まれHIPHOP育ち 悪い奴は大体友達</p>
+        <p>03line 福岡生まれHIPHOP育ち 悪い奴は大体友達</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
